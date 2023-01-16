0ae742c7122208760771f23048b70420aaeffc83
George <gex7777@gmail.com>
2023-01-16 10:33:20 +0530

    add card menu actions

---
+++ connections/src/components/CardMenuActions.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useStore } from "../state/store";
+interface Iprops {
+  userId: string;
+}
+export default function MenuActions({ userId }: Iprops) {
+  const deleteUser = useStore((state) => state.deleteUser);
+  const setUserIdToEdit = useStore((state) => state.setUserIdToEdit);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setUserIdToEdit(userId);
+    setAnchorEl(null);
+  };
+  const handleDelete = () => {
+    deleteUser(userId);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label="settings"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Edit</MenuItem>
+        <MenuItem onClick={handleDelete}>delete</MenuItem>
+      </Menu>
+    </div>
+  );
+}
