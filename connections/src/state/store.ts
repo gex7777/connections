a5b1433e7d9c7e09ef4f3837f1eda9f3b205d2c1
George <gex7777@gmail.com>
2023-01-19 14:05:30 +0530

    add graph

--- connections/src/state/store.ts
+++ connections/src/state/store.ts
@@ -1,11 +1,14 @@
 import { create } from "zustand";
 import { User } from "../components/UsersGrid";
 import { persist } from "zustand/middleware";
+import Graph from "@newdash/graphlib";
+
 type EditModalP = {
   id: string;
   data: Partial<User>;
 };
 type State = {
+  graph: Graph;
   showModal: boolean;
   users: User[];
   userIdToEdit: string | null;
@@ -44,24 +47,52 @@ function deleteUser(id: string, userArray: User[]): User[] {
 function addUser(data: User, userArray: User[]): User[] {
   return [...userArray, data];
 }
+function addNode(graph: Graph, payload: User): Graph {
+  graph.setNode(payload.id, { ...payload });
+  if (payload.friends.length !== 0) {
+    payload.friends.map((friend) => {
+      graph.setEdge(friend, payload.id);
+    });
+  }
+  return graph;
+}
+function deleteNode(graph: Graph, id: string): Graph {
+  graph.removeNode(id);
+  return graph;
+}
+function editNode(graph: Graph, id: string, data: Partial<User>): Graph {
+  graph.setNode(id, { id, ...data });
+  if (data.friends?.length !== 0) {
+    data.friends?.map((friend) => {
+      graph.setEdge(friend, id);
+    });
+  }
+  return graph;
+}
 export const useStore = create<State>()(
   persist(
     (set) => ({
+      graph: new Graph(),
       users: [],
       showModal: false,
       userIdToEdit: null,
       setUserIdToEdit: (payload) =>
         set(() => ({ userIdToEdit: payload, showModal: true })),
       addUser: (payload) =>
-        set((state) => ({ users: addUser(payload, state.users) })),
+        set((state) => ({
+          users: addUser(payload, state.users),
+          graph: addNode(state.graph, payload),
+        })),
       deleteUser: (payload) =>
         set((state) => ({
           users: deleteUser(payload, state.users),
+          graph: deleteNode(state.graph, payload),
         })),
       setShowModal: (payload) => set({ showModal: payload }),
       editUser: ({ id, data }) =>
         set((state) => ({
           users: updateUser(id, data, state.users),
+          graph: editNode(state.graph, id, data),
           userIdToEdit: null,
           showModal: false,
         })),
