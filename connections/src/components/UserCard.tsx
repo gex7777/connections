0431fdcdef8477057678ad3dbca31e4a182f0612
George <gex7777@gmail.com>
2023-02-16 19:45:57 +0530

    simplify card

--- connections/src/components/UserCard.tsx
+++ connections/src/components/UserCard.tsx
@@ -3,7 +3,14 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Avatar, CardActionArea, CardHeader, Stack } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  CardActionArea,
+  CardActions,
+  CardHeader,
+  Stack,
+} from "@mui/material";
 import { User } from "./UsersGrid";
 
 import Diversity3Icon from "@mui/icons-material/Diversity3";
@@ -16,40 +23,43 @@ export default function UserCard({ user }: Props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader action={<MenuActions userId={user.id} />} />
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={`https://robohash.org/${user.id}size=300x300`}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Stack direction={"row"} justifyContent={"space-between"}>
-            <div>
+
+      <CardMedia
+        component="img"
+        height="140"
+        image={`https://robohash.org/${user.id}size=300x300`}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <div>
+            <Typography gutterBottom variant="h5" component="div">
+              {user.name}
+            </Typography>
+          </div>
+          <div>
+            <Stack direction={"row-reverse"}>
+              <Diversity3Icon />
               <Typography gutterBottom variant="h5" component="div">
-                {user.name}
+                {user.friends.length}
               </Typography>
-            </div>
-            <div>
-              <Stack direction={"row-reverse"}>
-                <Diversity3Icon />
-                <Typography gutterBottom variant="h5" component="div">
-                  {user.friends.length}
-                </Typography>
-              </Stack>
-              <Stack direction={"row"}>
-                {user.friends.map((friend) => (
-                  <Avatar
-                    key={friend}
-                    alt="Remy Sharp"
-                    src={`https://robohash.org/${friend}size=300x300`}
-                  />
-                ))}
-              </Stack>
-            </div>
-          </Stack>
-        </CardContent>
-      </CardActionArea>
+            </Stack>
+            <Stack direction={"row"}>
+              {user.friends.map((friend) => (
+                <Avatar
+                  key={friend}
+                  alt="Remy Sharp"
+                  src={`https://robohash.org/${friend}size=300x300`}
+                />
+              ))}
+            </Stack>
+          </div>
+        </Stack>
+      </CardContent>
+
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Button size="small">Select</Button>
+      </CardActions>
     </Card>
   );
 }
