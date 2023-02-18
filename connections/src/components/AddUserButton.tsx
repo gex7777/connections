d806e1039ba6cd3fbce041e0b9a996a3dae96feb
George <gex7777@gmail.com>
2023-02-18 19:48:38 +0530

    add popup to component

--- connections/src/components/AddUserButton.tsx
+++ connections/src/components/AddUserButton.tsx
@@ -1,22 +1,28 @@
 import { Fab } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 import AddIcon from "@mui/icons-material/Add";
 import { useStore } from "./../state/store";
+import PopupForm from "./PopupForm copy";
 export const AddUserButton = () => {
-  const setShowModal = useStore((store) => store.setShowModal);
+  const [showModal, setShowModal] = useState(false);
   return (
-    <Fab
-      onClick={() => setShowModal(true)}
-      variant="extended"
-      sx={{
-        position: "fixed",
-        bottom: 16,
-        right: 16,
-      }}
-    >
-      <AddIcon sx={{ mr: 1 }} />
-      Add User
-    </Fab>
+    <>
+      <Fab
+        onClick={() => setShowModal(true)}
+        variant="extended"
+        sx={{
+          position: "fixed",
+          bottom: 16,
+          right: 16,
+        }}
+      >
+        <AddIcon sx={{ mr: 1 }} />
+        Add User
+      </Fab>
+      {showModal && (
+        <PopupForm opener={showModal} setShowOpener={setShowModal} type="ADD" />
+      )}
+    </>
   );
 };
