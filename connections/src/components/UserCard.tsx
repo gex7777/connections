bdd7c235568b2654b2bb18132b301d262ecef38f
George <gex7777@gmail.com>
2023-02-22 22:19:30 +0530

    change on select

--- connections/src/components/UserCard.tsx
+++ connections/src/components/UserCard.tsx
@@ -16,15 +16,22 @@ import { User } from "./UsersGrid";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import MenuActions from "./CardMenuActions";
 import { useStore } from "../state/store";
+import { useEffect } from "react";
 interface Props {
   user: User;
 }
 
 export default function UserCard({ user }: Props) {
   const addSelected = useStore((state) => state.addSelected);
-
+  const selected = useStore((state) => state.selectedUsers);
+  useEffect(() => console.log(selected), [selected]);
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        bgcolor: selected.includes(user.id) ? "burlywood" : "",
+      }}
+    >
       <CardHeader action={<MenuActions userId={user.id} />} />
 
       <CardMedia
@@ -62,7 +69,7 @@ export default function UserCard({ user }: Props) {
 
       <CardActions sx={{ justifyContent: "center" }}>
         <Button onClick={() => addSelected(user)} size="small">
-          Select
+          {selected.includes(user.id) ? "Unselect" : "Select"}
         </Button>
       </CardActions>
     </Card>
