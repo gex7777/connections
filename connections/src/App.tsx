831110ee51a1db628f1b7cbe7d5aceece72c4959
George <gex7777@gmail.com>
2023-02-22 22:19:13 +0530

    add compare button

--- connections/src/App.tsx
+++ connections/src/App.tsx
@@ -3,13 +3,17 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import { AddUserButton } from "./components/AddUserButton";
 import DefaultAppBar from "./components/AppBar";
+import { useStore } from "./state/store";
+import CompareFab from "./components/CompareFab";
 
 function App() {
+  const selected = useStore((state) => state.selectedUsers);
   return (
     <>
       <CssBaseline />
       <DefaultAppBar />
       <UsersGrid />
+      {selected.length === 2 && <CompareFab />}
       <AddUserButton />
     </>
   );
